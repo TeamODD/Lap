9fc5353dc4184ebe28fac982745293ea8448c32e
thdrlghks <rlghks7156@naver.com>
2025-05-16 19:13:57 +0900

    .
    .

---
+++ ODDLap/Assets/Character/UIScripts/NextImage.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NextImage : MonoBehaviour
+{
+    public GameObject NextIMG;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NextIMG.SetActive(true);
+            gameObject.SetActive(false);
+            return;
+        }
+    }
+
+
+}
--- ODDLap/Assets/Scriptsjh/Menu2.cs
+++ ODDLap/Assets/Scriptsjh/Menu2.cs
@@ -3,17 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class Menu2 : MonoBehaviour
 {
+    public Character targetScript; // 정지시키려는 스크립트
     public GameObject AdviceUI;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.L))
         {
+            targetScript.enabled = false;
             Time.timeScale = 0;
             AdviceUI.SetActive(true);
         }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            targetScript.enabled = true;
             Time.timeScale = 1;
             AdviceUI.SetActive(false);
         }
