6f2d7ffad018a25791e7d51e5787ad7ed1ec5d56
yfd0909 <yfd0909@naver.com>
2025-05-16 19:14:21 +0900

    마무리 수정
    메인메뉴, 인트로, 아웃트로 수정

---
+++ ODDLap/Assets/Scripts/LogoAnim.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class LogoAnim : MonoBehaviour
+{
+    public float amplitude = 30f; // 이동 높이 (위아래 총 60)
+    public float speed = 1f;      // 속도 (진동 주기)
+
+    private RectTransform rectTransform;
+    private Vector2 startPos;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        startPos = rectTransform.anchoredPosition;
+    }
+
+    void Update()
+    {
+        float offsetY = Mathf.Sin(Time.time * speed) * amplitude;
+        rectTransform.anchoredPosition = startPos + new Vector2(0, offsetY);
+    }
+}
