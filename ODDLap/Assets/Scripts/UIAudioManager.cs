80d1d312616f879bbdef882424c69f687ba5e823
TM-hub00 <univtm@hansung.ac.kr>
2025-05-17 00:42:01 +0900

    0516

---
+++ ODDLap/Assets/Scripts/UIAudioManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip buttonClickSound;
+    public AudioClip sliderTickSound;
+
+    private float lastSliderValue = -1f; // 슬라이더 초기값 체크용
+    public float sliderThreshold = 0.05f; // 이 값 이상 변화했을 때만 소리 재생
+
+    // 버튼에 연결할 함수
+    public void PlayButtonClickSound()
+    {
+        if (audioSource && buttonClickSound)
+            audioSource.PlayOneShot(buttonClickSound);
+    }
+
+
+    // 슬라이더에 연결할 함수
+    public void OnSliderChanged(float value)
+    {
+        if (Mathf.Abs(value - lastSliderValue) >= sliderThreshold)
+        {
+            lastSliderValue = value;
+
+            if (audioSource && sliderTickSound)
+                audioSource.PlayOneShot(sliderTickSound);
+        }
+    }
+
+}
