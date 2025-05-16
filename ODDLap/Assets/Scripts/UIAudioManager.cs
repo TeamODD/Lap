7fc52d0c15822f7c4d316432775ecc0030c6d15e
thdrlghks <rlghks7156@naver.com>
2025-05-17 02:25:56 +0900

    .
    .

--- ODDLap/Assets/Scripts/UIAudioManager.cs
+++ ODDLap/Assets/Scripts/UIAudioManager.cs
@@ -9,7 +9,26 @@ public class UIAudioManager : MonoBehaviour
 
     private float lastSliderValue = -1f; // 슬라이더 초기값 체크용
     public float sliderThreshold = 0.05f; // 이 값 이상 변화했을 때만 소리 재생
+    public Slider bgmSlider;
 
+    void Start()
+    {
+        // 초기값을 현재 볼륨에서 가져옴
+        if (SoundManager.Instance != null && SoundManager.Instance.musicAudioSource != null)
+        {
+            bgmSlider.value = SoundManager.Instance.musicAudioSource.volume;
+        }
+
+        // 슬라이더 변경 시 볼륨 변경
+        bgmSlider.onValueChanged.AddListener(SetVolume);
+    }
+    public void SetVolume(float value)
+    {
+        if (SoundManager.Instance != null && SoundManager.Instance.musicAudioSource != null)
+        {
+            SoundManager.Instance.musicAudioSource.volume = value;
+        }
+    }
     // 버튼에 연결할 함수
     public void PlayButtonClickSound()
     {
@@ -21,6 +40,11 @@ public class UIAudioManager : MonoBehaviour
     // 슬라이더에 연결할 함수
     public void OnSliderChanged(float value)
     {
+        if (audioSource)
+        {
+            audioSource.volume = value;
+        }
+
         if (Mathf.Abs(value - lastSliderValue) >= sliderThreshold)
         {
             lastSliderValue = value;
